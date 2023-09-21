c81e91c7920fbd933d9a79a2f300159da2898221
Иван <zhuldyb@MacBook-Air-Ivan.local>
2023-09-21 09:12:33 +0300

    zz

--- 5-languages/translater.js
+++ 5-languages/translater.js
@@ -1,13 +1,12 @@
 const language = prompt ("Введите необходимый язык");
 switch (language)  {
     case "de":
-    console.log("Guten Tag");
-    break;
+        console.log("Guten Tag");
+        break;
     case "en":
         console.log("Hello!");
         break;
-        case "ru":
-            console.log("Приветик");
-            break;
+    case "ru":
+        console.log("Приветик");
+        break;
     }
-    
