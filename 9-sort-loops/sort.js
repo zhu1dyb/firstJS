e982b467efed26145b74457026da848ea627368a
Иван <zhuldyb@MacBook-Air-Ivan.local>
2023-10-10 15:06:35 +0300

    исправил

--- 9-sort-loops/sort.js
+++ 9-sort-loops/sort.js
@@ -1,14 +1,30 @@
 let arr = [1, 40, -5, 10, 0];
 function sortArray (arr) {
-  for (let j = 0; j < arr.length; j++) {
-    for (let i = 0; i < arr.length - 1; i++) {
-      if (arr[i] > arr[i + 1]) {
-      let temp = arr[i];
-      arr[i] = arr[i + 1];
-      arr[i + 1] = temp
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+      [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
     }
   }
 }
   console.log(arr);
 }
 sortArray(arr);
+
+
+
+
+// let arr = [1, 40, -5, 10, 0];
+// function sortArray (arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length - 1; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//       let temp = arr[j];
+//       arr[j] = arr[j + 1];
+//       arr[j + 1] = temp
+//     }
+//   }
+// }
+//   console.log(arr);
+// }
+// sortArray(arr);
