44144c0d9a825bbcc584126f1960f467b8475c89
Иван <zhuldyb@MacBook-Air-Ivan.local>
2023-11-06 11:38:27 +0300

    Все понял, дошло до меня, все исправил

--- 15-task-object/task.js
+++ 15-task-object/task.js
@@ -1,50 +1,48 @@
 "use strict"
 
 const toDoList = {
-    title1: 
-    {
+    tasks: 
+    [{
     title: "Замена колес",
     id: 1,
     priority: 1
-    },
-    addTitle: function(theTitle) {
-            let nameTheTitle = prompt("Введите ключ title")
-            this[nameTheTitle] = {
-            title: theTitle,
-            id: Number(prompt("Введите id")),
-            priority: Number(prompt("Введите приоритет"))
-        }
+    }],
+
+    addTitle: function() {
+        let idOfT = this.tasks.length + 1;
+        this["tasks"].push(
+            {
+                title: prompt("Введите название задачи"),
+                id: idOfT,
+                priority: Number(prompt("Введите приоритет"))
+            }
+        )
+
         return this
         
     },
     deleteTitleWithId: function(idOfTitle) {
-        for(let key in this) {
-            if(this[key].id == idOfTitle) {
-                delete this[key]
+       for(let i = 0; i < toDoList.tasks.length; i++){
+            if(toDoList.tasks[i].id == idOfTitle) 
+                toDoList.tasks.splice(i, 1)
             }
-        }
         return this
     },
     refreshNameOfTitileById: function(idOfTitleToRefresh, nameOfTitle) {
-        for(let key in this) {
-            if(this[key].id == idOfTitleToRefresh) {
-                this[key].title = nameOfTitle
+        for(let i = 0; i < toDoList.tasks.length; i++) {
+            if(toDoList.tasks[i].id == idOfTitleToRefresh) {
+                toDoList.tasks[i].title = nameOfTitle
             }
         }
         return this
     },
     sortByPriority: function() {
-        const keys = Object.keys(this);
-        const titleKeys = keys.filter(key => key.includes("title"));
-        const sortedKeys = titleKeys.sort((a, b) => this[a].priority - this[b].priority);
-        const sortedList = sortedKeys.map(key => this[key]);
-        return sortedList;
+        this.tasks.sort((a, b) => a.priority - b.priority);
+        return this;
     }
 }
-toDoList.addTitle("Помыть окна")
-toDoList.addTitle("Собрать шкаф")
-toDoList.addTitle("Заменить батарейку")
-toDoList.deleteTitleWithId(prompt("Укажите id по которому удалить"))
-toDoList.refreshNameOfTitileById(prompt("Введите id для обновления имени"), "Cделать дз")
-toDoList.sortByPr4iority()
-
+toDoList.addTitle()
+console.log(toDoList.addTitle())
+console.log(toDoList.deleteTitleWithId(prompt("Укажите id по которому удалить")))
+console.log(toDoList.refreshNameOfTitileById(prompt("Введите id для обновления имени"), "Cделать дз"))
+console.log(toDoList.sortByPriority())
